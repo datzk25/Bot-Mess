e118df2e2ed47c6aacfe433dc3e355ef5afa406c
Tiến Đạt <130561352+datzk25@users.noreply.github.com>
2025-02-11 04:13:08 +0700

    Update appstate.js

--- appstate.js
+++ appstate.js
@@ -1,1 +1,128 @@
-//thích thì add appstate vô để cho nó chạy command
+[
+    {
+        "key": "datr",
+        "value": "KiVuZ2EHL-u-nzQ_ledO-HEL",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-02-10T17:29:55.982Z",
+        "lastAccessed": "2025-02-10T17:29:55.982Z"
+    },
+    {
+        "key": "sb",
+        "value": "KyVuZx8vclhDQy2hYXG6QXTx",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-02-10T17:29:55.982Z",
+        "lastAccessed": "2025-02-10T17:29:55.982Z"
+    },
+    {
+        "key": "c_user",
+        "value": "61570408226139",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-02-10T17:29:55.982Z",
+        "lastAccessed": "2025-02-10T17:29:55.982Z"
+    },
+    {
+        "key": "fr",
+        "value": "0wYUg3csEEsRHKDoA.AWVEp6t-jQjBPu8zOdp-MGRcCAw.BnbiUr..AAA.0.0.BnbiVY.AWU0uuFQNnE",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-02-10T17:29:55.982Z",
+        "lastAccessed": "2025-02-10T17:29:55.982Z"
+    },
+    {
+        "key": "xs",
+        "value": "31%3ABCJY1iZs0xPgmg%3A2%3A1735271768%3A-1%3A6276",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-02-10T17:29:55.982Z",
+        "lastAccessed": "2025-02-10T17:29:55.982Z"
+    },
+    {
+        "key": "ps_l",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-02-10T17:29:55.982Z",
+        "lastAccessed": "2025-02-10T17:29:55.982Z"
+    },
+    {
+        "key": "ps_n",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-02-10T17:29:55.982Z",
+        "lastAccessed": "2025-02-10T17:29:55.982Z"
+    },
+    {
+        "key": "dpr",
+        "value": "3.0234789848327637",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-02-10T17:29:55.982Z",
+        "lastAccessed": "2025-02-10T17:29:55.983Z"
+    },
+    {
+        "key": "presence",
+        "value": "C%7B%22lm3%22%3A%22g.8866036150181073%22%2C%22t3%22%3A%5B%7B%22o%22%3A0%2C%22i%22%3A%22g.7754693524655846%22%7D%5D%2C%22utc3%22%3A1739208560795%2C%22v%22%3A1%7D",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-02-10T17:29:55.983Z",
+        "lastAccessed": "2025-02-10T17:29:55.983Z"
+    },
+    {
+        "key": "wd",
+        "value": "357x709",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-02-10T17:29:55.983Z",
+        "lastAccessed": "2025-02-10T17:29:55.983Z"
+    },
+    {
+        "key": "locale",
+        "value": "vi_VN",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-02-10T17:29:55.983Z",
+        "lastAccessed": "2025-02-10T17:29:55.983Z"
+    },
+    {
+        "key": "fbl_st",
+        "value": "100434510%3BT%3A28986809",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-02-10T17:29:55.983Z",
+        "lastAccessed": "2025-02-10T17:29:55.983Z"
+    },
+    {
+        "key": "wl_cbv",
+        "value": "v2%3Bclient_version%3A2738%3Btimestamp%3A1739208581",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-02-10T17:29:55.983Z",
+        "lastAccessed": "2025-02-10T17:29:55.983Z"
+    },
+    {
+        "key": "vpd",
+        "value": "v1%3B780x393x2.75",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-02-10T17:29:55.983Z",
+        "lastAccessed": "2025-02-10T17:29:55.983Z"
+    }
+]
